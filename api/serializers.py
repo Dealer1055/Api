1931c2ddf6dc821bec60ac48337a993f5557b8f3
Dealer1055 <Dealer1055@abb923609>
2025-06-23 09:39:07 +0500

    123

--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,78 @@
 from rest_framework import serializers
 from .models import Author, Genre, Book
+from datetime import date
 
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
+class AuthorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    birth_date = serializers.DateField(required=False, allow_null=True)
 
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Genre
-        fields = '__all__'
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Ism kamida 3 harfdan iborat bo‘lishi kerak.")
+        return value
 
-class BookSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer()
-    genre = GenreSerializer(many=True)
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
 
-    class Meta:
-        model = Book
-        fields = '__all__'
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.birth_date = validated_data.get('birth_date', instance.birth_date)
+        instance.save()
+        return instance
+
+class GenreSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+
+    def validate_name(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Janr faqat harflardan iborat bo‘lishi kerak.")
+        return value
+
+    def create(self, validated_data):
+        return Genre.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=200)
+    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+    genre = serializers.PrimaryKeyRelatedField(queryset=Genre.objects.all(), many=True)
+    published_date = serializers.DateField()
+    price = serializers.DecimalField(max_digits=15, decimal_places=2)
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Narx 0 dan katta bo‘lishi kerak.")
+        return value
+
+    def validate_published_date(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("Nashr sanasi kelajakda bo‘lishi mumkin emas.")
+        return value
+
+    def validate(self, data):
+        if data['title'].lower() == data['author'].name.lower():
+            raise serializers.ValidationError("Kitob nomi muallif ismiga teng bo‘lmasligi kerak.")
+        return data
+
+    def create(self, validated_data):
+        genre_data = validated_data.pop('genre')
+        book = Book.objects.create(**validated_data)
+        book.genre.set(genre_data)
+        return book
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.published_date = validated_data.get('published_date', instance.published_date)
+        instance.price = validated_data.get('price', instance.price)
+        if 'genre' in validated_data:
+            instance.genre.set(validated_data['genre'])
+        instance.save()
+        return instance
